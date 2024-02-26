a2ea1de725f0cf419c65cbdcbe8b744dcaaa0c1f
Rahdin Hussain <60464006+Rahdi-H@users.noreply.github.com>
2024-02-26 17:30:22 +0600

    Update supabase_config.js

--- supabase_config.js
+++ supabase_config.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://acprtmcegjmctytrhkom.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjcHJ0bWNlZ2ptY3R5dHJoa29tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY4ODk4MjUsImV4cCI6MjAyMjQ2NTgyNX0.YPM2Wc5pr9oX7MeYlErumCn4vw58DyzuLJdofn6BDYw"
+const supabaseUrl = 'https://abcd.supabase.co'
+const supabaseKey = "abcd"
 const supabase = createClient(supabaseUrl, supabaseKey)
 export default supabase;
